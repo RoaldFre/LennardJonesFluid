320e89096389bfc6e0c4dba825cd34fb4fefc437
Roald Frederickx <roald.frederickx@gmail.com>
2011-12-18 13:57:01 +0100

    texified pressure plots

--- plotPressure.m
+++ plotPressure.m
@@ -1,11 +1,29 @@
-data = load("/tmp/pressure.txt");
+function plotPressure(filename, caption, T)
+close all;
 
-T = 2;
+data = load("/tmp/pressure.txt");
 
 rho = data(:,1);
 P = data(:,2);
 
-Pig = T * rho;
+rhoig = [0; 2*max(rho)];
+Pig = T * rhoig;
 
-plot(rho, P, rho, Pig, 'k');
+hold on;
+plot(rho,   P,   'b;Simulation;');
+plot(rhoig, Pig, 'k;Ideal gas;');
+legend('location', 'northwest');
+
+destdir   = 'latex/images';
+relImgDir = 'images';
+ylabrule  = '-1.5cm';
+xlab      = '$\rho$ (a.u.)';
+ylab      = '$P(\rho)$ (a.u.)';
+width     = '800';
+height    = '800';
+
+axis([0, 1.0*max(rho), 0, 1.0*max([P;Pig])]);
+
+makeGraph(filename,caption,destdir,relImgDir,xlab,ylab,ylabrule,width,height);
+hold off;
 
--- runPressure.sh
+++ runPressure.sh
@@ -2,28 +2,30 @@
 
 set -e
 
-numParticles=400
+numParticles=300
 LJcutoff=2.5
 samples=50
 measWait=10
 measInterval=0.15
 timestep=0.005
+temperature=2
 
 file=/tmp/pressure.txt
 
 echo -n "" > $file
 
-for density in `seq 0.01 0.05 1`
+for density in `seq 0.01 0.03 1`
 do
-	./main $numParticles $density -m P -r -l $LJcutoff -s $samples -w $measWait -i $measInterval -t $timestep
+	./main $numParticles $density -m P -r -l $LJcutoff -s $samples -w $measWait -i $measInterval -t $timestep -T $temperature
 
 	echo -n -e "$density\t" >> $file
 	cat /tmp/data.txt >> $file
 	echo "" >> $file
 done
-	
 
 
 
-
-octave --persist -q plotPressure.m
+info="\$N\$ = $numParticles, \$\Delta t\$ = $timestep, \$\\\\tau\$ = \$10^3 \Delta t\$, \$T\$ = $temperature, \$\\\\tau_\\\\mathrm{relax}\$ = $measWait, \$\\\\tau_\\\\mathrm{sample}\$ = $measInterval, \$N_\\\\mathrm{samples}\$ = $samples"
+caption="Pressure in function of density. Parameters: $info."
+filename="pressure"
+octave -q --eval "plotPressure('$filename', '$caption', $temperature)"
